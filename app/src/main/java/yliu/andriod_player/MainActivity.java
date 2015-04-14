73f98d78f3d8996cc442a5098a27b001729b2eb1
lyrato@gmail.com <lyrato@gmail.com>
2015-04-14 11:02:15 +0800

    add usb play

--- app/src/main/java/yliu/andriod_player/MainActivity.java
+++ app/src/main/java/yliu/andriod_player/MainActivity.java
@@ -1,25 +1,136 @@
 package yliu.andriod_player;
 
+import android.content.Context;
+import android.hardware.usb.UsbAccessory;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.net.Uri;
+import android.os.Environment;
 import android.widget.MediaController;
 import android.widget.VideoView;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+
+import java.io.File;
+import java.io.FilenameFilter;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String ACTIVITY_TAG="LogDemo";
+    public class MyFileFilter implements FilenameFilter {
 
+        @Override
+        public boolean accept(File directory, String fileName) {
+            if (fileName.endsWith(".mkv")) {
+                return true;
+            }
+            return false;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         VideoView vidView = (VideoView)findViewById(R.id.myVideo);
+        File[] file1 = null;
+         int max = 0;
+        //
+        File vSDCard = null;
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        List<String> usbAccessories = new ArrayList<String>();
+
+        UsbAccessory[] accessoryList = manager.getAccessoryList();
+        if (accessoryList != null) {
+            Log.e(MainActivity.ACTIVITY_TAG, ""+Thread.currentThread().getStackTrace()[2].getLineNumber());
+            usbAccessories.add(accessoryList[0].getSerial());
+        }
+        Log.e(MainActivity.ACTIVITY_TAG, ""+Thread.currentThread().getStackTrace()[2].getLineNumber());
+        Log.e(MainActivity.ACTIVITY_TAG, "" + accessoryList);
+        //UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+        HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
+        Iterator<UsbDevice> deviceIterator = deviceList.values().iterator();
+        while (deviceIterator.hasNext())
+        {
+            UsbDevice device = deviceIterator.next();
+            Log.e(MainActivity.ACTIVITY_TAG, "device name = " + device.getDeviceName());
+        }
+
+
+        try {
+            // 判斷 SD Card 有無插入
+            if( Environment.getExternalStorageState().equals(Environment.MEDIA_REMOVED) ) {
+                Log.e(MainActivity.ACTIVITY_TAG, ""+Thread.currentThread().getStackTrace()[2].getLineNumber());
+                return;
+            }
+            else
+            {
+                Log.e(MainActivity.ACTIVITY_TAG, ""+Thread.currentThread().getStackTrace()[2].getLineNumber());
+                // 取得 SD Card 位置
+                //vSDCard = Environment.getExternalStoragePublicDirectory(Environment.MEDIA_UNMOUNTABLE);
+                vSDCard = Environment.getExternalStorageDirectory();
+                Log.e(MainActivity.ACTIVITY_TAG, ""+vSDCard);
+            }
+
+            // 判斷目錄是否存在
+            File vPath = new File(  "/storage/external_storage/sda1/" );
+            //File vPath = new File(  vSDCard+"/" );
+            FilenameFilter filter = new MyFileFilter();
+
+             file1 = vPath.listFiles(filter);
+
+            int i = 0;
+            long max_byte = 0;
+            while(file1[i]!= null) {
+                Log.e(MainActivity.ACTIVITY_TAG, "" + file1[i].getName());
+                Log.e(MainActivity.ACTIVITY_TAG, "" + file1[i].length());
+                if(file1[i].length() >max_byte ) {
+                    max = i;
+                    max_byte = file1[i].length();
+                }
+                i++;
+
+            }
+            if( !vPath.exists() ) {
+                Log.e(MainActivity.ACTIVITY_TAG, ""+Thread.currentThread().getStackTrace()[2].getLineNumber());
+                vPath.mkdirs();
+            }
+
+            Log.e(MainActivity.ACTIVITY_TAG, ""+Thread.currentThread().getStackTrace()[2].getLineNumber());  // 寫入檔案
+            FileWriter vFile = new FileWriter(  "/storage/external_storage/sda1/myTest.txt" );
+            Log.e(MainActivity.ACTIVITY_TAG, ""+Thread.currentThread().getStackTrace()[2].getLineNumber());
+
+            vFile.write("Hello Android");
+            Log.e(MainActivity.ACTIVITY_TAG, ""+Thread.currentThread().getStackTrace()[2].getLineNumber());
+            vFile.close();
+            Log.e(MainActivity.ACTIVITY_TAG, "" + Thread.currentThread().getStackTrace()[2].getLineNumber());
+
+        } catch (Exception e) {
+            // 錯誤處理
+        }
+
+
+
+        ///
+        String usbAddress = file1[max].getAbsolutePath();
+        Log.e(MainActivity.ACTIVITY_TAG, "" + usbAddress);
         String vidAddress = "http://192.168.33.169/mediafile/%e5%9b%bd%e4%ba%a7%e9%9b%b6%e9%9b%b6%e6%bc%86.mkv";
-        Uri vidUri = Uri.parse(vidAddress);
+        Uri vidUri = Uri.parse(usbAddress);
         vidView.setVideoURI(vidUri);
-        vidView.start();
+        Log.e(MainActivity.ACTIVITY_TAG, ""+Thread.currentThread().getStackTrace()[2].getLineNumber());
+        //vidView.start();
     }
 
 
